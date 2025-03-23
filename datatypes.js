af88d77f2547ae0edf236fbabce4d03e5d27708a
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-23 16:59:09 +0500

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as a newer version
+
+// code readability should be high
+
+let name = "sarmad"
+let age  = 18
+let islogged = false
+
+// string use ""
+// null -> a standalone value
+// undefined -> value not assigned
+// symbol -> unique
