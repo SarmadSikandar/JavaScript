47540961050bc88941a721e8fc3d0902189f8bff
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-23 18:00:44 +0500

    conversion ops

--- conversionoperation.js
+++ conversionoperation.js
@@ -1,8 +1,13 @@
 let score = "33"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let convertedscore = Number(score)
 
-console.log(typeof convertedscore);
+// console.log(typeof convertedscore);
+
+//  Operations
+let value = 3
+let negvalue = -value
+console.log(negvalue)
 
