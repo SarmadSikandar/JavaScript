c10b190e52fbbff5a527cef304fda9d904014e96
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-23 18:11:29 +0500

    comparison JS

---
+++ comparison.js
@@ -0,0 +1,5 @@
+// Make sure while doing comparisons make variables of same data types otherwise you will get unpredictable results.
+
+// === strict check -> checks for both datatype and the value
+
+// console.log("2"===2);  the result would be false as it does no conversion and strictly checks them!!
