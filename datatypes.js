dd848fb00cd3182ded3892f90afe46f6759acf48
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-24 15:51:32 +0500

    datatypes

--- datatypes.js
+++ datatypes.js
@@ -10,3 +10,18 @@ let islogged = false
 // null -> a standalone value
 // undefined -> value not assigned
 // symbol -> unique
+
+// Primitive (Value)
+// String , Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "apple", "gogi"]
+
+let myobj = {   // object definition
+    name: "Sarmad",
+    age : 5
+}
+
+let myFunction = function(){} // function def
