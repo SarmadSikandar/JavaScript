fa68381daa08493967d5cb05a72d33af653dfe4a
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-27 12:43:57 +0500

    maths

---
+++ maths.js
@@ -0,0 +1,16 @@
+const balance = new Number(100)
+console.log(balance)
+console.log(balance.toString())
+
+// ++++++++++++++++++++ Maths ++++++++++++++++++++++++
+
+console.log(Math.abs(-4));
+
+// generating random number betweeen 1 and 6
+const min= 1;
+const max = 6;
+
+
+console.log(Math.floor(Math.random() * (max-min+1))+ min );
+
+
