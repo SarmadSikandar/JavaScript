b9a90477c7e4459faa97418f8459230b45f45ab1
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-27 12:18:04 +0500

    strings

---
+++ strings.js
@@ -0,0 +1,6 @@
+let name = "Sarmad Sikandar"
+
+console.log(`hello my name is ${name}`)
+
+const myname = new String("Sarmad") // gives the string object so we can access its funcs
+console.log(myname.toUpperCase())
