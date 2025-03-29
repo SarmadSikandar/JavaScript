0bbf6052482e4a31d9e5b06aa96d23294a088d0a
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-03-29 14:40:39 +0500

    functions

---
+++ functions.js
@@ -0,0 +1,14 @@
+function addnumbers(num1,num2)  // parameters
+{
+    // console.log(num1+num2);
+    
+}
+
+addnumbers(2,2)   // arguments
+
+function calculateCartPrice(...num1){   // rest / spread operator
+
+    return num1
+}
+
+console.log(calculateCartPrice(100,200,300,400,500));
