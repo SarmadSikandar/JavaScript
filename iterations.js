059a8e08b2843524e94e0c3e4f70873ecb2f3ca0
SarmadSikandar <sarmad.sikandar21@gmail.com>
2025-04-01 22:07:57 +0500

    iterations

---
+++ iterations.js
@@ -0,0 +1,29 @@
+// for loop
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    // console.log(element); 
+}
+
+// for of loop
+const arr = [1,2,3,4,5]
+for (const num of arr) {
+    // console.log(num);
+    
+}
+
+// for in loop
+const myobj = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "ruby"
+}
+for (const key in myobj) {
+// console.log(myobj[key]);
+}
+// for each loop
+const coding = ["js","ruby","java","python","cpp"]
+
+coding.forEach(function (item){
+    console.log(item)
+})
+   
